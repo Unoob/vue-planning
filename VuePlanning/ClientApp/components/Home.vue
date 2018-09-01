44f7341732cdb3a37efc198d951d622d4ad8f6c0
Łukasz Wójtowicz <lp.wojtowicz@outlook.com>
2018-09-01 16:17:25 +0200

    - first commit

--- VuePlanning/ClientApp/components/Home.vue
+++ VuePlanning/ClientApp/components/Home.vue
@@ -1,8 +1,42 @@
 ﻿<template>
-    <label>Vue Home</label>
+    <v-container fluid>
+        <v-layout row>
+            <v-flex xs12>
+                <v-text-field v-model="login" label="Login" placeholder="Login" solo></v-text-field>
+            </v-flex>
+        </v-layout>
+        <v-layout row>
+            <v-flex xs12>
+                <v-text-field v-model="roomCode" label="RoomCode" placeholder="Nr pokoju" solo></v-text-field>
+            </v-flex>
+        </v-layout>
+        <v-layout row>
+            <v-flex xs6>
+                <v-btn v-on:click="onCreateRoom">Stwórz</v-btn>
+            </v-flex>
+            <v-flex xs6>
+                <v-btn v-on:click="onJoinRoom">Dołącz</v-btn>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
+ 
 <script>
     export default {
-        name: "Home"
+        name: "Home",
+        data: function() {
+          return {
+            login: "",
+            roomCode: ""
+          }
+        },
+        methods: {
+            onCreateRoom: function () {
+                console.log('onCreateRoom');
+            },
+            onJoinRoom: function () {
+                console.log('onJoinRoom');
+            }
+        }
     };
 </script>
