74a6dc609ccda688cc660a05b6ad96a3242ecc05
Konrad KK. Kwieciński <konrad.kwiecinski@pirios.com>
2018-09-01 14:27:22 +0200

    Master Page

--- VuePlanning/ClientApp/components/App.vue
+++ VuePlanning/ClientApp/components/App.vue
@@ -1,21 +1,33 @@
 ﻿<template>
     <div id="app-root">
-        <router-view />
+        <v-app>
+            <MasterPage></MasterPage>
+        </v-app>
     </div>
 
 </template>
 
 <script>
     import { HubConnectionBuilder } from '@aspnet/signalr';
+    import MasterPage from './MasterPage.vue';
     const HUBS = {
         PLANNING: '/PlanningHub'
     };
 export default {
         name: "App",
+        components: {
+            MasterPage
+        },
         data:()=> {
             return {
                 connectionHub: {}
+                
             };
+        },
+        computed: {
+            connectionId: function() {
+                return new Date().getMilliseconds();
+            }
         },
         mounted: function () {
             this.connectionHub = new HubConnectionBuilder().withUrl(HUBS.PLANNING).build();
