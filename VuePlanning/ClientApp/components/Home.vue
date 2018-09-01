d15964e32463e8535a8068cd93df4575797b2fe9
Łukasz Wójtowicz <lp.wojtowicz@outlook.com>
2018-09-01 16:48:16 +0200

    changes 2

--- VuePlanning/ClientApp/components/Home.vue
+++ VuePlanning/ClientApp/components/Home.vue
@@ -1,21 +1,25 @@
 ﻿<template>
     <v-container fluid>
-        <v-layout row>
-            <v-flex xs12>
+        <v-layout row justify-space-around>
+            <v-flex xs4>
                 <v-text-field v-model="login" label="Login" placeholder="Login" solo></v-text-field>
             </v-flex>
         </v-layout>
-        <v-layout row>
-            <v-flex xs12>
+        <v-layout row justify-space-around>
+            <v-flex xs4>
                 <v-text-field v-model="roomCode" label="RoomCode" placeholder="Nr pokoju" solo></v-text-field>
             </v-flex>
         </v-layout>
-        <v-layout row>
-            <v-flex xs6>
-                <v-btn v-on:click="onCreateRoom">Stwórz</v-btn>
-            </v-flex>
-            <v-flex xs6>
-                <v-btn v-on:click="onJoinRoom">Dołącz</v-btn>
+        <v-layout row justify-center>
+            <v-flex xs4>
+                <v-layout row justify-space-around>
+                    <v-flex xs5 align-content-center>
+                        <v-btn block v-on:click="onCreateRoom">Stwórz</v-btn>
+                    </v-flex>
+                    <v-flex xs5 align-content-center>
+                        <v-btn block v-on:click="onJoinRoom">Dołącz</v-btn>
+                    </v-flex>
+                </v-layout>
             </v-flex>
         </v-layout>
     </v-container>
@@ -25,10 +29,10 @@
     export default {
         name: "Home",
         data: function() {
-          return {
-            login: "",
-            roomCode: ""
-          }
+            return {
+                login: "",
+                roomCode: ""
+            };
         },
         methods: {
             onCreateRoom: function () {
