46dfefee375ec46eacfdaac281cc88a2e5d490be
Łukasz Wójtowicz <lp.wojtowicz@outlook.com>
2018-09-10 16:20:51 +0200

    - value card component part1

--- VuePlanning/ClientApp/components/Answer.vue
+++ VuePlanning/ClientApp/components/Answer.vue
@@ -1,27 +1,43 @@
 <template>
     <v-container grid-list-lg>
-        <v-layout row wrap fill-height>
-            <v-flex xs12 sm6 md4 v-for="n in answers" :key="n">
-                <v-card>
-                    <v-card-text>
-                        <v-layout align-center>
-                            <v-flex text-xs-center>
-                                {{n}}
-                            </v-flex>
-                        </v-layout>
-                    </v-card-text>
-                </v-card>
+        <v-layout row wrap fill-height align-left>
+            <v-flex v-for="card in answers" :key="card.text">
+                <ValueCardComponent :card="card" @onValueCardClicked="onValueCardClicked"></ValueCardComponent>
             </v-flex>
         </v-layout>
     </v-container>
 </template>
+
 <script>
-export default {
-    name:"Answer",
-    data(){
-        return {
-            answers:['1/2','1','2','3','5','8','13','21','34','55']
+    import ValueCardComponent from "../components/ValueCard.vue";
+
+    export default {
+        name: "Answer",
+        components: { ValueCardComponent },
+        data() {
+            return {
+                //TODO: dodaæ id do obiektu, ¿eby w przypadku powielenia tekstu prawid³owo znajdowaæ elementy (unikalnoœc id)
+                answers: [new CardViewModel('1/2'), new CardViewModel('1'), new CardViewModel('2'), new CardViewModel('3'), new CardViewModel('5'), new CardViewModel('8'),
+                    new CardViewModel('13'), new CardViewModel('21'), new CardViewModel('34'), new CardViewModel('55')]
+            };
+        },
+        methods: {
+            onValueCardClicked: function (object) {
+                console.log("clicked parent!");
+                var arrayElement = this.answers.find(function (item) { return item === object; });
+                this.clearSelectedValue();
+                arrayElement.selected = !arrayElement.selected;
+            },
+            clearSelectedValue: function () {
+                var arrayElement = this.answers.find(function (item) { return item.selected === true; });
+                arrayElement = arrayElement || {};
+                arrayElement.selected = !arrayElement.selected;
+            }
         }
+    };
+
+    function CardViewModel(text) {
+        this.text = text;
+        this.selected = false;
     }
-}
 </script>
---
+++ VuePlanning/ClientApp/components/ValueCard.vue
@@ -0,0 +1,62 @@
+﻿<template>
+        <v-card @click.native="onValueCardClicked" :class="{'selected-class' : card.selected, 'card' : true}">
+            <v-card-text>
+                <v-layout align-center>
+                    <v-flex text-xs-center>
+                        {{card.text}}
+                    </v-flex>
+                </v-layout>
+            </v-card-text>
+        </v-card>
+</template>
+
+<script>
+    export default {
+        name: "ValueCard",
+        data() {
+            return {
+                
+            };
+        },
+        props: {
+            card: {
+                type: Object,
+                required: true,
+                validator: function (object) {
+                    if (!object.text)
+                        return false;
+                    if (typeof object.selected !== "boolean")
+                        return false;
+                    if (typeof object.text !== "string" || object.text.length < 0 || object.text.length > 4)
+                        return false;
+
+                    return true;
+                }
+            }
+        },
+        methods: {
+            onValueCardClicked: function () {
+                this.$emit("onValueCardClicked", this.card);
+            } 
+        }
+    };
+</script>
+
+<style lang="scss"> 
+    /*TODO: dostosować graficznie, narazie wygląda jeszcze przeciętnie*/
+
+    .card {
+        background: linear-gradient(to bottom right, #CCC, #AAA);
+        padding: 110px 5px;
+        color: white;
+        width: 200px;
+        font-size: 40px;
+        cursor: pointer;
+               
+        }
+
+    .selected-class {
+        background: green !important;
+    }
+
+</style>
